0b28727a42cec0102aafb0fec0f6b4d22f23a64a
iacoleite <caio.cobucci@gmail.com>
2024-07-16 19:26:12 +0200

    first commit

---
+++ sort/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+public class Program {
+    public static void Main(string[] args) {
+
+        int[] teste = [54,5643,34,345,465,764,235,142,21,234,3456,4675,3456,423,432,213,123,321,5342,543,4576,5687,5,4756,534,5432,2134,234,7465,867,6798,5867,678,5678,7465];
+        static int[] ordinaVettoriNum(int []num) {
+            for(int i = 0; i < num.Count(); i++) {
+                for (int j = i + 1; j < num.Count() - 1; j++) {
+                    if (num[j] < num[i]) {
+                        var temp = num[i];
+                        num[i] = num[j];
+                        num[j] = temp;
+                    }
+                }
+            }
+            return num;
+        }
+
+         int [] sortedArray = ordinaVettoriNum(teste);
+        // int lunghezza = sortedArray.Count();
+        // for (int i = 0; i < lunghezza; i++){
+        //     Console.WriteLine(ordinaVettoriNum(teste)[i]);
+        // }
+        
+         Console.WriteLine(sortedArray.ToString());
+
+    }
+}
