d40243f84635a06108c116bfd815420cd8be1805
Caio Leite <caio.cobucci@gmail.com>
2024-07-17 01:07:09 +0200

    Fixed string to print result

--- sort/Program.cs
+++ sort/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Net;
+using System.Text;
 
 public class Program {
     public static void Main(string[] args) {
 
         int[] teste = [54,5643,34,345,465,764,235,142,21,234,3456,4675,3456,423,432,213,123,321,5342,543,4576,5687,5,4756,534,5432,2134,234,7465,867,6798,5867,678,5678,7465];
+        
         static int[] ordinaVettoriNum(int []num) {
             for(int i = 0; i < num.Count(); i++) {
                 for (int j = i + 1; j < num.Count() - 1; j++) {
@@ -18,13 +20,20 @@ public class Program {
             return num;
         }
 
-         int [] sortedArray = ordinaVettoriNum(teste);
+        int [] sortedArray = ordinaVettoriNum(teste);
+
         // int lunghezza = sortedArray.Count();
         // for (int i = 0; i < lunghezza; i++){
         //     Console.WriteLine(ordinaVettoriNum(teste)[i]);
         // }
-        
-         Console.WriteLine(sortedArray.ToString());
 
+        StringBuilder resultBuilder = new StringBuilder();
+
+        foreach (int element in sortedArray) {
+            resultBuilder.Append(element + ", ");
+        }
+
+        Console.WriteLine(resultBuilder);
+        
     }
 }
